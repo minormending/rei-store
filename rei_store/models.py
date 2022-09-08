10fdc61c672a9b965819f378efc285cfbfa03d5a
minormending <54426030+minormending@users.noreply.github.com>
2022-09-08 16:28:35 -0400

    remove unused fields

--- rei_store/models.py
+++ rei_store/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Any, Dict, List
 
 
 @dataclass
@@ -8,3 +9,22 @@ class Category:
 
     def url(self) -> str:
         return f"https://www.rei.com{self.slug}"
+
+
+def trim_model(model: Any) -> Any:
+    if isinstance(model, list):
+        output: List[Any] = []
+        for value in model:
+            cleaned_value = trim_model(value)
+            if cleaned_value:
+                output.append(cleaned_value)
+        return output
+    elif isinstance(model, dict):
+        output: Dict[str, Any] = {}
+        for key,value in model.items():
+            cleaned_value = trim_model(value)
+            if cleaned_value:
+                output[key] = cleaned_value
+        return output
+    else:
+        return model
